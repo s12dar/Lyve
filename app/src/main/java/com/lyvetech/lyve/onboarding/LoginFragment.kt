ecbeb47e80e4d66290ba8404c957d266acf5945e
pserdax <parahatgeldiyew.s@gmail.com>
2021-08-29 18:16:27 +0300

    Changed Authentication mentality

--- app/src/main/java/com/lyvetech/lyve/onboarding/LoginFragment.kt
+++ app/src/main/java/com/lyvetech/lyve/onboarding/LoginFragment.kt
@@ -1,15 +1,76 @@
 package com.lyvetech.lyve.onboarding
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lyvetech.lyve.R
 import com.lyvetech.lyve.databinding.FragmentLoginBinding
+import com.lyvetech.lyve.datamodels.User
 
 class LoginFragment : Fragment() {
 
+    private var TAG = LoginFragment::class.qualifiedName
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var maAuth: FirebaseAuth
+
+    // Validate each field in the form with the same watcher
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(
+            charSequence: CharSequence,
+            i: Int,
+            i1: Int,
+            i2: Int
+        ) {
+        }
+
+        override fun onTextChanged(
+            charSequence: CharSequence,
+            i: Int,
+            i1: Int,
+            i2: Int
+        ) {
+        }
+
+        override fun afterTextChanged(editable: Editable) {
+            when {
+                editable === binding.etLoginEmail.editableText -> {
+                    val email = binding.etLoginEmail.text.toString().trim()
+                    if (email.isBlank()) {
+                        // Setting the error on the layout is important to make the properties work. Kotlin synthetics are being used here
+                        binding.tilLoginEmail.error =
+                            getString(R.string.err_empty_field)
+                    } else {
+                        binding.tilLoginEmail.error = null
+                    }
+                }
+                editable === binding.etLoginPassword.editableText -> {
+                    val password = binding.etLoginPassword.text.toString().trim()
+                    if (password.isBlank()) {
+                        // Setting the error on the layout is important to make the properties work. Kotlin synthetics are being used here
+                        binding.tilLoginPassword.error =
+                            getString(R.string.err_empty_field)
+                    } else {
+                        binding.tilLoginPassword.error = null
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        maAuth = Firebase.auth
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +79,50 @@ class LoginFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.etLoginEmail.addTextChangedListener(watcher)
+        binding.etLoginPassword.addTextChangedListener(watcher)
+
+        binding.btnSignIn.setOnClickListener {
+            val email = binding.etLoginEmail.text.toString().trim()
+            val password = binding.etLoginPassword.text.toString().trim()
+
+            if (email.isEmpty()) {
+                binding.tilLoginEmail.error =
+                    getString(R.string.err_empty_field)
+                return@setOnClickListener
+            }
+            if (password.isEmpty()) {
+                binding.tilLoginPassword.error =
+                    getString(R.string.err_empty_field)
+                return@setOnClickListener
+            }
+
+            signIn(email, password)
+        }
+
+        binding.tvJoinUs.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
         return binding.root
+    }
+
+    private fun signIn(
+        email: String,
+        password: String
+    ) {
+        binding.progressBar.visibility = View.VISIBLE
+        maAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this.requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmailAndPassword:success")
+                    val user: User
+                } else {
+                    Log.w(TAG, "signInWithEmailAndPassword:failure", task.exception)
+                    Toast.makeText(context, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/lyvetech/lyve/onboarding/RegisterFragment.kt
+++ app/src/main/java/com/lyvetech/lyve/onboarding/RegisterFragment.kt
@@ -1,15 +1,19 @@
 package com.lyvetech.lyve.onboarding
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.lyvetech.lyve.R
 import com.lyvetech.lyve.databinding.FragmentRegisterBinding
 import com.lyvetech.lyve.datamodels.User
 import java.util.*
@@ -20,48 +24,110 @@ class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
     private lateinit var mAuth: FirebaseAuth
 
+
+    // Validate each field in the form with the same watcher
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(
+            charSequence: CharSequence,
+            i: Int,
+            i1: Int,
+            i2: Int
+        ) {
+        }
+
+        override fun onTextChanged(
+            charSequence: CharSequence,
+            i: Int,
+            i1: Int,
+            i2: Int
+        ) {
+        }
+
+        override fun afterTextChanged(editable: Editable) {
+            when {
+                editable === binding.etRegisterEmail.editableText -> {
+                    val email = binding.etRegisterEmail.text.toString().trim()
+                    if (email.isBlank()) {
+                        // Setting the error on the layout is important to make the properties work. Kotlin synthetics are being used here
+                        binding.tilRegisterEmail.error =
+                            getString(R.string.err_empty_field)
+                    } else {
+                        binding.tilRegisterEmail.error = null
+                    }
+                }
+                editable === binding.etRegisterPassword.editableText -> {
+                    val password = binding.etRegisterPassword.text.toString().trim()
+                    if (password.isBlank()) {
+                        // Setting the error on the layout is important to make the properties work. Kotlin synthetics are being used here
+                        binding.tilRegisterPassword.error =
+                            getString(R.string.err_empty_field)
+                    } else {
+                        binding.tilRegisterPassword.error = null
+                    }
+                }
+                editable === binding.etRegisterConfirmPassword.editableText -> {
+                    val confirmPassword = binding.etRegisterConfirmPassword.text.toString().trim()
+                    if (confirmPassword.isBlank()) {
+                        // Setting the error on the layout is important to make the properties work. Kotlin synthetics are being used here
+                        binding.tilRegisterConfirmPassword.error =
+                            getString(R.string.err_empty_field)
+                    } else {
+                        binding.tilRegisterConfirmPassword.error = null
+                    }
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mAuth = Firebase.auth
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
 
-        binding.btnCreateAccount.setOnClickListener { view ->
+        binding.etRegisterEmail.addTextChangedListener(watcher)
+        binding.etRegisterPassword.addTextChangedListener(watcher)
+        binding.etRegisterConfirmPassword.addTextChangedListener(watcher)
 
-            val name = binding.etName.text.toString().trim()
-            val familyName = binding.etName.text.toString().trim()
-            val email = binding.etEmail.text.toString().trim()
-            val password = binding.etPassword.text.toString().trim()
-            val phoneNumber = binding.etPhoneNumber.text.toString().trim()
+        binding.btnCreateAccount.setOnClickListener {
 
-            if (name.isEmpty()) {
-                binding.etName.error = "Oops, this field couldn't be empty"
-                return@setOnClickListener
-            }
-            if (familyName.isEmpty()) {
-                binding.etFamilyName.error = "Oops, this field couldn't be empty"
-                return@setOnClickListener
-            }
+            val email = binding.etRegisterEmail.text.toString().trim()
+            val password = binding.etRegisterPassword.text.toString().trim()
+            val confirmPassword = binding.etRegisterConfirmPassword.text.toString().trim()
+
             if (email.isEmpty()) {
-                binding.etEmail.error = "Oops, this field couldn't be empty"
+                binding.tilRegisterEmail.error =
+                    getString(R.string.err_empty_field)
                 return@setOnClickListener
             }
             if (password.isEmpty()) {
-                binding.etPassword.error = "Oops, this field couldn't be empty"
+                binding.tilRegisterPassword.error =
+                    getString(R.string.err_empty_field)
                 return@setOnClickListener
             }
-            if (phoneNumber.isEmpty()) {
-                binding.etPhoneNumber.error = "Oops, this field couldn't be empty"
+            if (confirmPassword.isEmpty()) {
+                binding.tilRegisterConfirmPassword.error =
+                    getString(R.string.err_empty_field)
                 return@setOnClickListener
             }
 
-            createAccount(email, password, name, familyName, phoneNumber)
+            if (password == confirmPassword) {
+                createAccount(email, password)
+            } else {
+                binding.tilRegisterConfirmPassword.error =
+                    getString(R.string.err_password_match)
+            }
+        }
+
+        binding.tvSignIn.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
         }
 
 
@@ -72,9 +138,6 @@ class RegisterFragment : Fragment() {
     private fun createAccount(
         email: String,
         password: String,
-        name: String,
-        lastName: String,
-        phoneNumber: String
     ) {
         binding.progressBar.visibility = View.VISIBLE
         mAuth.createUserWithEmailAndPassword(email, password)
