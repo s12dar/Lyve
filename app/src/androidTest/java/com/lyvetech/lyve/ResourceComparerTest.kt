45d3dc7eda2fc70f01baf1f69cc77e6d11a874b4
pserdax <parahatgeldiyew.s@gmail.com>
2021-10-03 19:16:26 +0300

    Unit test practicing

---
+++ app/src/androidTest/java/com/lyvetech/lyve/ResourceComparerTest.kt
@@ -0,0 +1,31 @@
+package com.lyvetech.lyve
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class ResourceComparerTest {
+
+    private lateinit var resourceComparer: ResourceComparer
+
+    @Before
+    fun setUp() {
+        resourceComparer = ResourceComparer()
+    }
+
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Lyve")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnsFalse() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context, R.string.app_name, "Hello")
+        assertThat(result).isFalse()
+    }
+}
---
+++ app/src/main/java/com/lyvetech/lyve/ResourceComparer.kt
@@ -0,0 +1,10 @@
+package com.lyvetech.lyve
+
+import android.content.Context
+
+class ResourceComparer {
+
+    fun isEqual(context: Context, resId: Int, string: String): Boolean {
+        return context.getString(resId) == string
+    }
+}
