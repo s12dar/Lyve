a74f4da0ef21563dd91cc4ca891fb131d3b3966e
pserdax <parahatgeldiyew.s@gmail.com>
2021-11-13 16:23:12 +0200

    Added app bar instead of bottom nav. Customized bottom sheet in adding activity.

--- app/src/main/java/com/lyvetech/lyve/ui/fragments/main/HomeFragment.kt
+++ app/src/main/java/com/lyvetech/lyve/ui/fragments/main/HomeFragment.kt
@@ -2,17 +2,18 @@ package com.lyvetech.lyve.ui.fragments.main
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.lyvetech.lyve.R
 import com.lyvetech.lyve.application.LyveApplication
 import com.lyvetech.lyve.databinding.FragmentHomeBinding
 import com.lyvetech.lyve.datamanager.DataListener
 import com.lyvetech.lyve.datamanager.DataManager
 import com.lyvetech.lyve.datamodels.Activity
-import java.lang.Exception
 import java.util.*
 
 class HomeFragment : Fragment() {
@@ -32,9 +33,11 @@ class HomeFragment : Fragment() {
         Log.i(TAG, "onCreateView")
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
         // Declare elements for activities recyclerview
         lateinit var linearLayoutManager: LinearLayoutManager
         lateinit var homeAdapter: HomeAdapter
+
         DataManager.mInstance.getActivities(object : DataListener<List<Activity?>> {
             override fun onData(data: List<Activity?>?, exception: Exception?) {
                 if (data != null) {
@@ -51,6 +54,17 @@ class HomeFragment : Fragment() {
             }
         })
 
+        binding.fabAdd.setOnClickListener {
+            showBottomSheetDialog()
+        }
+
         return binding.root
+    }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetDialog = context?.let { BottomSheetDialog(it) }
+        bottomSheetDialog!!.setContentView(R.layout.bottom_sheet_create_activity)
+
+        bottomSheetDialog.show()
     }
 }
