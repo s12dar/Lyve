bc9e56c19cf9b2114345119ac2ebc3d40c689045
Serdar Parahatgeldiyev <serdar.2.parahatgeldiyev@conti-engineering.com>
2022-03-09 11:48:22 +0200

    Resource modifications

--- app/src/main/java/com/lyvetech/lyve/utils/Resource.kt
+++ app/src/main/java/com/lyvetech/lyve/utils/Resource.kt
@@ -1,23 +1,7 @@
 package com.lyvetech.lyve.utils
 
-data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
-    companion object {
-        fun <T> success(data: T?): Resource<T> {
-            return Resource(Status.SUCCESS, data, null)
-        }
-
-        fun <T> error(msg: String, data: T?): Resource<T> {
-            return Resource(Status.ERROR, data, msg)
-        }
-
-        fun <T> loading(data: T?): Resource<T> {
-            return Resource(Status.LOADING, data, null)
-        }
-    }
-}
-
-enum class Status {
-    SUCCESS,
-    ERROR,
-    LOADING
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Success<T>(data: T?) : Resource<T>(data)
+    class Error<T>(data: T? = null, message: String?) : Resource<T>(data, message)
 }
